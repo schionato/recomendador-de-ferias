085b131839882142a29b6598d9cfa314442c4ce9
Bruno Schionato <schionato@gmail.com>
2018-01-29 21:43:04 -0200

    Calculando a quantidade de dias

---
+++ src/main/java/CalculadorDeFerias.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class CalculadorDeFerias {
+
+    private final int diasSolicitados;
+    private final Periodo periodoASerAnalisado;
+    private final Map<Long, List<Periodo>> periodos;
+
+    CalculadorDeFerias(int diasSolicitados, Periodo periodoASerAnalisado) {
+        this.diasSolicitados = diasSolicitados;
+        this.periodoASerAnalisado = periodoASerAnalisado;
+        this.periodos = new HashMap<>();
+    }
+
+    CalculadorDeFerias calcula() {
+        List<Periodo> subPeriodos = periodoASerAnalisado.gerarSubPeriodos(diasSolicitados);
+
+        for (Periodo periodo : subPeriodos) {
+            long quantidadeDiasUteis = periodo.getQuantidadeDeDiasUteis();
+
+            this.periodos.computeIfAbsent(quantidadeDiasUteis, aLong -> {
+                List<Periodo> periodos = new ArrayList<>();
+                periodos.add(periodo);
+                return periodos;
+            });
+
+            this.periodos.computeIfPresent(quantidadeDiasUteis, (quantidade, periodos) -> {
+                periodos.add(periodo);
+                return periodos;
+            });
+        }
+        return this;
+    }
+
+    public List<Periodo> getMelhorPeriodo() {
+        long maiorPeriodo = this.periodos.keySet().stream().mapToLong(quantidade -> quantidade).max().getAsLong();
+        return this.periodos.get(maiorPeriodo);
+    }
+}
---
+++ src/test/java/CalculadorDeFeriasTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculadorDeFeriasTest {
+
+    @Test
+    public void verificaMelhorPeriodo() {
+        Dia dataInicial = new Dia("01/01/2018");
+        Dia dataFinal = new Dia("31/01/2018");
+
+        Periodo periodoASerAnalisado = new Periodo(dataInicial, dataFinal);
+
+        List<Periodo> periodo = new CalculadorDeFerias(10, periodoASerAnalisado)
+                .calcula()
+                .getMelhorPeriodo();
+
+        assertEquals(2, periodo.size());
+        assertEquals(8, periodo.get(0).getQuantidadeDeDiasUteis());
+        assertEquals(8, periodo.get(1).getQuantidadeDeDiasUteis());
+
+        Periodo periodo1 = periodo.get(0);
+    }
+}
